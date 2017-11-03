3083fe543f39f1ca8a77c3652121be5ced6eccc6
Emma Stout <emma.stout01@gmail.com>
2017-11-03 14:30:40 -0500

    add riddle template on dom

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,13 @@
-var myApp = angular.module( 'myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
+
+
+// Configuring routes for templates
+myApp.config(function ($routeProvider) {
+    $routeProvider.when('/add', {
+        templateUrl: 'templates/add.html',
+        controller: 'AddRiddleController as add'
+    }).when('/view', {
+        templateUrl: 'templates/view.html',
+        controller: 'ViewRiddleController as view'
+    })
+})
